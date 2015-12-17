262558f31d18d58a9f872c4f8b239413118775c7
Scott Schwartz <sschwa12@gmail.com>
2015-12-17 11:20:43 -0600

    fix row and col logic

--- sudoku-checker.js
+++ sudoku-checker.js
@@ -1,25 +1,26 @@
-var boardStr = 
-"735814296\n" +
-"896275314\n" +
-"214963857\n" +
-"589427163\n" +
-"362189745\n" +
-"471356982\n" +
-"923541678\n" +
-"648792531\n" +
-"157638429\n";
+var boardStr =
+  "735814296\n" +
+  "896275314\n" +
+  "214963857\n" +
+  "589427163\n" +
+  "362189745\n" +
+  "471356982\n" +
+  "923541678\n" +
+  "648792531\n" +
+  "157638429";
 
-function sudokuCheck (boardStr) {
+function sudokuCheck(boardStr) {
+  var result = 'solved';
   var board = boardStr.split('\n').map(function(row) {
     return row.split('').map(function(num) {
       return num;
     });
   });
-  
+
   // Check Rows
-  board.map(function(row) {
+  board.forEach(function(row) {
     if (!validate(row)) {
-      return 'invalid'
+      result = 'invalid';
     };
   });
 
@@ -27,7 +28,7 @@ function sudokuCheck (boardStr) {
   var cols = [];
   for (var i = 0; i < 9; i++) {
     cols.push(board.map(function(row) {
-        return row[i];
+      return row[i];
     }));
   }
   cols.map(function(col) {
@@ -37,19 +38,24 @@ function sudokuCheck (boardStr) {
   })
 
   // Check 3x3s
-  var threes = [];
-  
+  //     var threes = [];
+  //     var threes = board.map(function(row) {
+  //         return row.slice(0, 3)
+  //     })
 
-  return 'solved';
+  //     console.log(threes);
+  return result;
 }
 
 function validate(arr) {
   var arrCopy = arr.slice();
   arrCopy = JSON.stringify(arrCopy.sort());
   var checkAgainst = JSON.stringify(['1', '2', '3', '4', '5', '6', '7', '8', '9']);
+  //     console.log(arrCopy === checkAgainst);
   if (arrCopy === checkAgainst) {
     return true;
+  } else {
+    return false;
   }
-  return false;
 }
 sudokuCheck(boardStr);
