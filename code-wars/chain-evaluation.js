f92914e99c14281f14b2ed47a0e20fdbbba71c7a
Scott Schwartz <sschwa12@gmail.com>
2016-01-04 09:33:03 -0500

    chain evaluation

---
+++ code-wars/chain-evaluation.js
@@ -0,0 +1,22 @@
+// http://www.codewars.com/kata/545a5dd961aa4c0987000980/train/javascript
+
+
+Number.prototype.add = function(number) {
+  return this + number;
+}
+
+Number.prototype.subtract = function(number) {
+  return this - number;
+}
+
+Number.prototype.multiply = function(number) {
+  return this * number;
+}
+
+Number.prototype.divide = function(number) {
+  return this / number;
+}
+
+Number.prototype.square = function() {
+  return this * this;
+}
