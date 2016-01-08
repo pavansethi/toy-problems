ac1ffd75c964fdbe6fcf1aa61c6da301c84f5a79
Scott Schwartz <sschwa12@gmail.com>
2016-01-08 14:39:53 -0500

    two-sum completed

---
+++ code-wars/two-sum.js
@@ -0,0 +1,20 @@
+/*
+Write a function that takes an array of numbers (integers for the tests) and a target number. It should find two different items in the array that, when added together, give the target value. The indices of these items should then be returned in an array like so: [index1, index2].
+*/
+
+function twoSum(numbers, target) {
+  var storage = {};
+
+  for (var i = 0; i < numbers.length; i++) {
+      var diff = target - numbers[i];
+      if (storage[diff] === undefined) {
+          storage[numbers[i]] = i;
+      } else {
+          console.log(storage);
+          return [storage[diff], i]
+      }
+  }
+  return false;
+}
+
+twoSum([1234,5678,9012], 14690)
