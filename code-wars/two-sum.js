9a1384b19624863e905a77a0b9077ae0ee78abf0
Scott Schwartz <sschwa12@gmail.com>
2016-01-12 16:46:37 -0500

    updates to two-sum

--- code-wars/two-sum.js
+++ code-wars/two-sum.js
@@ -2,6 +2,12 @@
 Write a function that takes an array of numbers (integers for the tests) and a target number. It should find two different items in the array that, when added together, give the target value. The indices of these items should then be returned in an array like so: [index1, index2].
 */
 
+/*
+Write a function that takes an array of numbers (integers for the tests) and a target number. It should find two different items in the array that, when added together, give the target value. The indices of these items should then be returned in an array like so: [index1, index2].
+*/
+
+
+// Return indices
 function twoSum(numbers, target) {
   var storage = {};
 
@@ -16,4 +22,40 @@ function twoSum(numbers, target) {
   return false;
 }
 
-twoSum([1234,5678,9012], 14690);
+twoSum([1,4,6,8,2,5,9], 10)
+
+
+// Return values
+var twoSumWithValues = function(numbers, target) {
+  var storage = {};
+
+  for (var i = 0; i < numbers.length; i++) {
+    var diff = target - numbers[i];
+
+    if (storage[diff] === undefined) {
+      storage[numbers[i]] = numbers[i];
+    } else {
+      return [storage[diff], numbers[i]];
+    }
+  }
+}
+
+twoSumWithValues([1,4,6,8,2,5,9], 10)
+
+// Return all values that add to target
+var twoSumWithAllValues = function(numbers, target) {
+  var storage = {};
+  var results = [];
+
+  for (var i = 0; i < numbers.length; i++) {
+    var diff = target - numbers[i];
+    if (storage[diff] === undefined) {
+      storage[numbers[i]] = numbers[i];
+    } else {
+      results.push([storage[diff], numbers[i]]);
+    }
+  }
+  return JSON.stringify(results);
+}
+
+twoSumWithAllValues([1, 4, 6, 8, 2, 5, 9], 10)
